077674492db1da463e347b1763758c614c51f83c
Dave Kiss <hello@davekiss.com>
2014-01-28 16:56:22 -0600

    Check heroku logging

--- app/controllers/spree/paypal_button_controller.rb
+++ app/controllers/spree/paypal_button_controller.rb
@@ -13,7 +13,7 @@ module Spree
     end
 
     def notify
-      binding.pry
+      logger.info "Heroku logging enabled"
       if provider.ipn_valid?(request.raw_post)  # return true or false
 
         @order = Spree::Order.find_by(order_number: ipn_params[:custom])
