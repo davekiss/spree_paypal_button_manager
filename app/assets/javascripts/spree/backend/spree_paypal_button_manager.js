0ef4170f82c3bb7c6fa22913dc2705574c1aefd6
Dave Kiss <hello@davekiss.com>
2014-05-19 11:20:20 -0500

    Add a backend payment form when creating a new order

--- app/assets/javascripts/spree/backend/spree_paypal_button_manager.js
+++ app/assets/javascripts/spree/backend/spree_paypal_button_manager.js
@@ -1,1 +1,25 @@
 //= require spree/backend
+
+SpreePaypalButton = {
+  hideSettings: function(paymentMethod) {
+    if (SpreePaypalButton.paymentMethodID && paymentMethod.val() == SpreePaypalButton.paymentMethodID) {
+      $('.payment-method-settings').children().hide();
+      $('#payment_amount').prop('disabled', 'disabled');
+      $('button[type="submit"]').prop('disabled', 'disabled');
+      $('#paypal-warning').show();
+    } else if (SpreePaypalButton.paymentMethodID) {
+      $('.payment-method-settings').children().show();
+      $('button[type=submit]').prop('disabled', '');
+      $('#payment_amount').prop('disabled', '')
+      $('#paypal-warning').hide();
+    }
+  }
+}
+
+$(document).ready(function() {
+  checkedPaymentMethod = $('[data-hook="payment_method_field"] input[type="radio"]:checked');
+  SpreePaypalButton.hideSettings(checkedPaymentMethod);
+  paymentMethods = $('[data-hook="payment_method_field"] input[type="radio"]').click(function (e) {
+    SpreePaypalButton.hideSettings($(e.target));
+  });
+})
