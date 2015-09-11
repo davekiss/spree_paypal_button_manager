ff9673ba27b84888cb81fa50274b1b13024c8d53
Dave Kiss <dave@davekiss.com>
2015-09-11 15:35:57 -0400

    Implement refunding for PayPal Merchant API through Spree's reimbursement interface

--- app/models/spree/gateway/pay_pal_button.rb
+++ app/models/spree/gateway/pay_pal_button.rb
@@ -53,17 +53,49 @@ module Spree
       end.new
     end
 
-    # Probably doesn't work
-    def refund(payment, amount)
-      refund_type = payment.amount == amount.to_f ? "Full" : "Partial"
-      refund_transaction = provider.build_refund_transaction({
-        :TransactionID => payment.source.transaction_id,
-        :RefundType => refund_type,
-        :Amount => {
+    def credit(credit_cents, response_code, event)
+      payment = event[:originator].payment
+      reimbursement = event[:originator].reimbursement
+
+      refund_type = (payment.amount * 100).to_i == credit_cents ? "Full" : "Partial"
+
+      transaction = {
+        :TransactionID     => payment.source.transaction_id,
+        :PayerID           => payment.source.payer_id,
+        :InvoiceID         => payment.order.number,
+        :RefundType        => refund_type,
+        :RefundSource      => "any",
+        :Memo              => event[:originator].reason.name,
+        :RefundItemDetails => refund_items(reimbursement),
+        :MsgSubID => ('a'..'z').to_a.shuffle[0,8].join
+      }
+
+      # Only set refund amount if partial transaction
+      if refund_type == "Partial"
+        transaction[:Amount] = {
           :currencyID => payment.currency,
-          :value => amount },
-        :RefundSource => "any" })
-      refund_transaction_response = provider.refund_transaction(refund_transaction)
+          :value      => '%.2f' % reimbursement.total
+        }
+      end
+
+      refund_transaction = merchant.build_refund_transaction(transaction)
+      refund_transaction_response = merchant.refund_transaction(refund_transaction)
+
+      # [2] pry(#<Spree::Gateway::PayPalButton>)> refund_transaction_response
+      # => #<PayPal::SDK::Merchant::DataTypes::RefundTransactionResponseType:0x007fb7813ca560
+      #  @Ack="Success",
+      #  @Build="000000",
+      #  @CorrelationID="806cdcc8ae9fc",
+      #  @FeeRefundAmount=#<PayPal::SDK::Merchant::DataTypes::BasicAmountType:0x007fb7813c9340 @currencyID="USD", @value="1.87">,
+      #  @GrossRefundAmount=#<PayPal::SDK::Merchant::DataTypes::BasicAmountType:0x007fb7813c8e68 @currencyID="USD", @value="64.50">,
+      #  @MsgSubID="vndfspmt",
+      #  @NetRefundAmount=#<PayPal::SDK::Merchant::DataTypes::BasicAmountType:0x007fb7813c96b0 @currencyID="USD", @value="62.63">,
+      #  @RefundInfo=#<PayPal::SDK::Merchant::DataTypes::RefundInfoType:0x007fb7813c8670 @PendingReason="none", @RefundStatus="Instant">,
+      #  @RefundTransactionID="64099041HJ4472300",
+      #  @Timestamp=Fri, 11 Sep 2015 18:42:46 +0000,
+      #  @TotalRefundedAmount=#<PayPal::SDK::Merchant::DataTypes::BasicAmountType:0x007fb7813c8ad0 @currencyID="USD", @value="64.50">,
+      #  @Version="106.0">
+
       if refund_transaction_response.success?
         payment.source.update_attributes({
           :refunded_at => Time.now,
@@ -72,10 +104,50 @@ module Spree
           :refund_type => refund_type
         })
       end
-      refund_transaction_response
+
+      # https://github.com/activemerchant/active_merchant/blob/86e84518d591bc9435b86e6505c509be822960c0/lib/active_merchant/billing/response.rb
+      return ActiveMerchant::Billing::Response.new(
+        refund_transaction_response.success?,
+        refund_transaction_response.errors.map(&:long_message).join(" ")
+      )
     end
+
+    private
+
+      def refund_items(reimbursement)
+        items = []
+        reimbursement.return_items.each do |return_item|
+          line_item = return_item.inventory_unit.line_item
+          variant   = return_item.inventory_unit.variant
+
+          items << {
+            :Name => variant.name,
+            :SKU  => variant.sku,
+            :Price => {
+              :currencyID => line_item.currency,
+              :value      => '%.2f' % return_item.pre_tax_amount
+            },
+            :ItemCount => line_item.quantity,
+            :Discount  => refund_item_discounts(line_item),
+            :Taxable   => reimbursement.order.tax_zone.present?
+          }
+        end
+        items
+      end
+
+      def refund_item_discounts(line_item)
+        discounts = []
+        line_item.adjustments.eligible.each do |adj|
+          discounts << {
+            :Name    => adj.label,
+            :Amount  => {
+              :currencyID => adj.currency,
+              :value      => '%.2f' % adj.amount.to_f.abs
+            }
+          }
+        end
+        discounts
+      end
+
   end
 end
-
-#   payment.state = 'completed'
-#   current_order.state = 'complete'
